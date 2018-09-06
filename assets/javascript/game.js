7f3e5aa6fa6e2830d3cc84da6cdd22d2249170b9
MattsPoche <MatthewsPoche@comcast.net>
2018-09-05 21:12:38 -0500

    logic, css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,14 @@ $(document).ready(function(){
             this.hp = hp;
             this.ap = ap;
             this.cap= cap;
-            this.name = name;            
+            this.name = name;
+            this.numAtt = 1;            
         }
         getHp(){
             return this.hp;
         }
         getAp(){
-            return this.ap;
+            return this.ap * this.numAtt;
         }
         getCap(){
             return this.cap;
@@ -19,12 +20,18 @@ $(document).ready(function(){
         getName(){
             return this.name;
         }
+        getNumAtt(){
+            return this.numAtt;
+        }
         defend(attack){
             this.hp -= attack;
         }
         setAp(ap){
             this.ap = ap;
-        }        
+        }   
+        setNumAtt(numAtt){
+            this.numAtt = numAtt;
+        }     
     };
 
     //gameState enum
@@ -41,37 +48,72 @@ $(document).ready(function(){
             var fdiv = $("<div>");
             fdiv.addClass("fighter");
             fdiv.attr("data-index", i);
-            fdiv.text(fighters[i].getName());
+            fdiv.append("<div id='name'>"+fighters[i].getName()+"</div>");
+            fdiv.append("<div id='hp'> Health: "+fighters[i].getHp()+"</div>");
+            fdiv.append("<div id='ap'> Attack: "+fighters[i].getAp()+"</div>");
+            fdiv.append("<div id='cap'> Counter Attack: "+fighters[i].getCap()+"</div>");
 
             $("#bench").append(fdiv);
         }
     };
+    
+    //tests if an element is empty
+    var isEmpty = function( el ){
+        return !$.trim(el.html());
+    }
+
+    //displays ui text
+    var displayText = function(fighters){
+        $(".fighter").each(function(index){
+            var f = fighters[$(this).attr("data-index")];
+            $(this).children("#hp").text("Health: "+f.getHp());
+            $(this).children("#ap").text("Attack: "+f.getAp());
+            $(this).children("#cap").text("Counter Attack: "+f.getCap());
+            switch(state){
+                case gameState.PLAYER_SELECT:
+                    $("#info").text("Choose your Character!!");
+                    break;
+                case gameState.DEFENDER_SELECT:
+                    $("#info").text("Choose your Opponent!!");
+                    break;
+                case gameState.BATTLE:
+                    $("#info").text("Attack!!");
+                    break;
+                case gameState.GAME_END:
+                    $("#info").text("Game Over");
+                    break;
+            }
+
+        });
+    };
 
     //global vars
     var fighters = [];
-    fighters.push(new Fighter(100, 4, 6, "Luke Skywalker"));
+    fighters.push(new Fighter(100, 5, 6, "Luke Skywalker"));
     fighters.push(new Fighter(120, 6, 6, "Obi-Wan Kenobi"));
     fighters.push(new Fighter(150, 10, 7, "Darth Sidious"));
-    fighters.push(new Fighter(180, 10, 10, "Darth Vader"));
+    fighters.push(new Fighter(180, 10, 9, "Darth Vader"));
     let state = gameState.PLAYER_SELECT;
     addFighters(fighters);
-    
-    
-    
-    
+
     //on-click events
-    $(".fighter").on("click", (event) => {
-        switch(state){
-            case gameState.PLAYER_SELECT:
-                var player = $(event.target);            
-                $("#yourCharacter").append(player);
-                state = gameState.DEFENDER_SELECT;
-                break;
-            case gameState.DEFENDER_SELECT:
-                var defender = $(event.target);
-                $("#defender").append(defender);
-                state = gameState.BATTLE;
-                break;
+    $(".fighter").on("click", function(){
+        if($(this).parent().attr("id") === "bench"){
+            if(state != gameState.GAME_END){
+                switch(state){
+                    case gameState.PLAYER_SELECT:
+                        var player = $(this);            
+                        $("#yourCharacter").append(player);
+                        state = gameState.DEFENDER_SELECT;
+                        break;
+                    case gameState.DEFENDER_SELECT:
+                        var defender = $(this);
+                        $("#defender").append(defender);
+                        state = gameState.BATTLE;
+                        break;
+                }
+                displayText(fighters);
+            }
         }
     });
     $("#attackButton").on("click", function(){
@@ -79,15 +121,25 @@ $(document).ready(function(){
             var player = $("#yourCharacter").children(".fighter").attr("data-index");
             var defender = $("#defender").children(".fighter").attr("data-index");
             fighters[defender].defend(fighters[player].getAp());
-            fighters[player].setAp(fighters[player].getAp() * 2);
+            fighters[player].setNumAtt(fighters[player].getNumAtt()  + 1);
             fighters[player].defend(fighters[defender].getCap());
-            console.log(fighters[player].getHp()+" "+fighters[defender].getHp());
+            
+            //Conditionals
             if(fighters[player].getHp() <= 0){
                 $("#yourCharacter").children(".fighter").remove();
+                state = gameState.GAME_END;
             }else if(fighters[defender].getHp() <= 0){
                 $("#defender").children(".fighter").remove();
                 state = gameState.DEFENDER_SELECT;
             }
+            //console.log(isEmpty($("#defender")));
+            console.log($("#bench").children().length );
+            console.log( $("#defender").children().length);
+            if( $("#bench").children().length === 0 && $("#defender").children().length === 1 ){
+                state = gameState.GAME_END;
+            }
+            
+            displayText(fighters);
         }
     });
 });
